3271e4cffab5e7c5056778d501ba8e4d60f574b3
creador30 <creador@abwesend.de>
2017-06-22 08:37:35 -0700

    updates

---
+++ quality/quality/doctype/proses_kontrol_formu/proses_kontrol_formu.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2016, ESO Electronic (Nemwatik) and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Proses Kontrol Formu', {
+	refresh: function(frm) {
+
+	}
+});
---
+++ quality/quality/doctype/proses_kontrol_formu/test_proses_kontrol_formu.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2015, ESO Electronic (Nemwatik) and Contributors
+# See license.txt
+from __future__ import unicode_literals
+
+import frappe
+import unittest
+
+# test_records = frappe.get_test_records('Proses Kontrol Formu')
+
+class TestProsesKontrolFormu(unittest.TestCase):
+	pass
