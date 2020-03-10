686abf408ec357f6c1b5e3a471db54f3bcba64fc
creador30 <creador@abwesend.de>
2020-03-10 18:03:40 +0300

    Update hooks.py

--- quality/hooks.py
+++ quality/hooks.py
@@ -15,7 +15,7 @@ app_license = "MIT"
 # ------------------
 
 # include js, css files in header of desk.html
-# app_include_css = "/assets/quality/css/quality.css"
+app_include_css = "/files/custom.css"
 # app_include_js = "/assets/quality/js/quality.js"
 
 # include js, css files in header of web template
